7a091108b95d1fac10979cee727c50d2381e62a8
Tyler King <stking74@gmail.com>
2018-10-29 15:49:17 -0400

    Comment and docstring for scrape_directory()
    Add descriptive comments and docstring to scrape_directory() definition

--- my_utils.py
+++ my_utils.py
@@ -11,26 +11,36 @@ def scrape_directory(path, flag, recursive=True):
     '''
     import os
     catalog = []
-    dir_dump = os.listdir(path)
+    #Dump all contents of path into temporary container
+    dir_dump = os.listdir(path)         
+    #Iterate through temporary container, categorize contents as files or directories
     for item in dir_dump:
         fullname = os.path.join(path, item)
+        #If item is a file, append the absolute filename to catalog
         if os.path.isfile(fullname):
             catalog.append(fullname)
+        #If item is a subdirectory...
         elif os.path.isdir(fullname):
+            #If recursive mode is enabled, recursively calls scrape_directory to read and categorize contents
             if recursive:
                 contents = scrape_directory(fullname, flag)
                 for entry in contents:
                     catalog.append(entry)
+            #If recursive mode is disabled, passes over subdirectory
             else:
                 pass
         else:
             pass
     returned_files = []
+    #Iterate through items identified as files
     for item in catalog:
+        #Split item to extract file extension
         split_item = item.split('.')
         try:
+            #If extracted extension matches flag, append item to list of items to be returned
             if split_item[1] == flag:
                 returned_files.append(item)
+        #If item has no associated extension, pass over item
         except IndexError:
             continue
     return returned_files
